8c8d952de478505377d9a865d0bd1647764e4201
alxayeed <alxayeed@gmail.com>
2022-09-20 22:37:31 +0600

    buildGroceryScreen added to show list or emtpy grocery screen

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/models/models.dart';
 import 'package:fooderlich/screens/empty_grocery_screen.dart';
+import 'package:provider/provider.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    //TODO 4: Add scaffold
-    return EmptyGroceryScreen();
+    return Scaffold(
+      body: buildGroceryScreen(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //TODO 11: present groceryItemScreen
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
-  //TODO: Add buildGroceryScreen
+
+  Widget buildGroceryScreen() {
+    return Consumer<GroceryManager>(builder: (context, groceryManager, child) {
+      if (groceryManager.groceryItems.isNotEmpty) {
+        // TODO 25: Add GroceryListScreen
+        return Container();
+      } else {
+        return const EmptyGroceryScreen();
+      }
+    });
+  }
 }
