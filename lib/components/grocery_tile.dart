675303deebd1102503e4df7fae2fc9ad6ad17a97
alxayeed <alxayeed@gmail.com>
2022-09-21 23:44:21 +0600

    grocery tile completed

--- lib/components/grocery_tile.dart
+++ lib/components/grocery_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fooderlich/models/models.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class GroceryTile extends StatelessWidget {
   final GroceryItem groceryItem;
@@ -16,17 +18,96 @@ class GroceryTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //TODO 21: Change this widget
-    return Container(
+    return SizedBox(
       height: 100.0,
-      color: Colors.purple,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 5.0,
+                color: groceryItem.color,
+              ),
+              const SizedBox(
+                width: 16.0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    groceryItem.name,
+                    style: GoogleFonts.lato(
+                        decoration: textDecoration,
+                        fontSize: 21.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 4.0,
+                  ),
+                  buildDate(),
+                  const SizedBox(
+                    height: 4.0,
+                  ),
+                  buildImportance()
+                ],
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                groceryItem.quantity.toString(),
+                style: GoogleFonts.lato(
+                  decoration: textDecoration,
+                  fontSize: 21.0,
+                ),
+              ),
+              buildCheckbox()
+            ],
+          )
+        ],
+      ),
     );
   }
 
-  // TODO: Add buildImportance()
+  Widget buildImportance() {
+    //TODO: replace with switch statement
+    if (groceryItem.importance == Importance.low) {
+      return Text(
+        'Low',
+        style: GoogleFonts.lato(decoration: textDecoration),
+      );
+    } else if (groceryItem.importance == Importance.medium) {
+      return Text(
+        'Medium',
+        style: GoogleFonts.lato(
+            fontWeight: FontWeight.w800, decoration: textDecoration),
+      );
+    } else if (groceryItem.importance == Importance.high) {
+      return Text(
+        'High',
+        style: GoogleFonts.lato(
+            fontWeight: FontWeight.w900, decoration: textDecoration),
+      );
+    } else
+      (throw Exception('This Importance type does not exist'));
+  }
 
-  // TODO: Add buildDate()
+  Widget buildDate() {
+    final dateFormatter = DateFormat('MMMM dd, hh:mm a'); //TODO: learn more
+    final dateString = dateFormatter.format(groceryItem.date);
+    return Text(
+      dateString,
+      style: TextStyle(decoration: textDecoration),
+    );
+  }
 
-  // TODO: Add buildCheckbox()
-
+  Widget buildCheckbox() {
+    return Checkbox(
+      value: groceryItem.isComplete,
+      onChanged: onComplete,
+    );
+  }
 }
--- lib/screens/grocery_item_screen.dart
+++ lib/screens/grocery_item_screen.dart
@@ -4,6 +4,7 @@ import 'package:fooderlich/components/grocery_tile.dart';
 import 'package:fooderlich/models/models.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
 
 class GroceryItemScreen extends StatefulWidget {
   final Function(GroceryItem) onCreate;
@@ -67,7 +68,25 @@ class _GroceryItemScreenState extends State<GroceryItemScreen> {
         actions: [
           IconButton(
               onPressed: () {
-                //TODO 24: Add callback handler
+                final groceryItem = GroceryItem(
+                  id: widget.originalItem?.id ?? const Uuid().v1(),
+                  name: _nameController.text,
+                  importance: _importance,
+                  color: _currentColor,
+                  quantity: _currentSliderValue,
+                  date: DateTime(
+                    _dueDate.year,
+                    _dueDate.month,
+                    _dueDate.day,
+                    _timeOfDay.hour,
+                    _timeOfDay.minute,
+                  ),
+                );
+                if (widget.isUpdating) {
+                  widget.onUpdate(groceryItem);
+                } else {
+                  widget.onCreate(groceryItem);
+                }
               },
               icon: const Icon(Icons.check)),
         ],
