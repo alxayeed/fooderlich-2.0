ebf00908d6a061696a0a43ef02a4876ba1b69e02
alxayeed <alxayeed@gmail.com>
2022-09-22 00:09:30 +0600

    InkWell added to update grocery item

--- lib/screens/grocery_list_screen.dart
+++ lib/screens/grocery_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fooderlich/components/grocery_tile.dart';
 import 'package:fooderlich/models/models.dart';
+import 'package:fooderlich/screens/grocery_item_screen.dart';
 
 class GroceryListScreen extends StatelessWidget {
   final GroceryManager groceryManager;
@@ -19,13 +20,27 @@ class GroceryListScreen extends StatelessWidget {
         itemBuilder: (context, index) {
           final item = groceryItems[index];
           //TODO: Wrap in Dismissable and InkWell
-          return GroceryTile(
-            key: Key(item.id),
-            groceryItem: item,
-            onComplete: (change) {
-              if (change != null) {
-                groceryManager.completeItem(index, change);
-              }
+          return InkWell(
+            child: GroceryTile(
+                key: Key(item.id),
+                groceryItem: item,
+                onComplete: (change) {
+                  if (change != null) {
+                    groceryManager.completeItem(index, change);
+                  }
+                }),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => GroceryItemScreen(
+                            originalItem: item,
+                            onCreate: (item) {},
+                            onUpdate: (item) {
+                              groceryManager.updateItem(item, index);
+                              Navigator.pop(context);
+                            },
+                          )));
             },
           );
         },
