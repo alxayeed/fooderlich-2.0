af48669243bbc2c3bf2e2d7c41f4bb894be8c57f
alsayeed <alxayeed@gmail.com>
2022-10-23 21:58:57 +0600

    app router added

---
+++ lib/navigation/app_router.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/models/models.dart';
+import 'package:fooderlich/screens/screens.dart';
+
+class AppRouter extends RouterDelegate
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin {
+  @override
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  final AppStateManager appStateManager;
+  final ProfileManager profileManager;
+  final GroceryManager groceryManager;
+
+  AppRouter({
+    required this.appStateManager,
+    required this.profileManager,
+    required this.groceryManager,
+  }) : navigatorKey = GlobalKey<NavigatorState>() {
+    appStateManager.addListener(notifyListeners);
+    profileManager.addListener(notifyListeners);
+    groceryManager.addListener(notifyListeners);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    appStateManager.removeListener(notifyListeners);
+    profileManager.removeListener(notifyListeners);
+    groceryManager.removeListener(notifyListeners);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: navigatorKey,
+      onPopPage: _handlePopPage,
+      pages: [
+        // TODO: Add SplashScreen
+        // TODO: Add LoginScreen
+        // TODO: Add OnBoardingScreen
+        // TODO: Add Home
+        // TODO: create new item
+        // TODO: Select GroceryItemScreen
+        // TODO: Add ProfileScreen
+        // TODO: Add WebView Screen
+      ],
+    );
+  }
+
+  bool _handlePopPage(Route<dynamic> route, result) {
+    if (!route.didPop(result)) {
+      return false;
+    }
+
+    // TODO: Handle onBoarding and Splash
+    // TODO: Handle state when user closes grocery item screen
+    // TODO: Handle state when user closes profile screen
+    // TODO: Handle state when user closes WebView screen
+
+    return true;
+  }
+
+  @override
+  Future<void> setNewRoutePath(configuration) async => null;
+}
