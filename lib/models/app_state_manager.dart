e032eb57ffad9596878f05981795087c0b6870c0
alsayeed <alxayeed@gmail.com>
2022-10-23 18:54:44 +0600

    app state manager added

---
+++ lib/models/app_state_manager.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class FooderlichTab {
+  static const int explore = 0;
+  static const int recipes = 1;
+  static const int toBuy = 2;
+}
+
+class AppStateManager extends ChangeNotifier {
+  bool _initialized = false;
+  bool _loggedIn = false;
+  bool _onBoardingComplete = false;
+
+  int _selectedTab = FooderlichTab.explore;
+
+  bool get isInitialized => _initialized;
+  bool get isLoggedIn => _loggedIn;
+  bool get isOnBoardingComplete => _onBoardingComplete;
+  int get selectedTab => _selectedTab;
+
+  void initializeApp() {
+    Timer(const Duration(microseconds: 2000), () {
+      _initialized = true;
+      notifyListeners();
+    });
+  }
+
+  void login(String username, String password) {
+    _loggedIn = true;
+    notifyListeners();
+  }
+
+  void completeOnBoarding() {
+    _onBoardingComplete = true;
+    notifyListeners();
+  }
+
+  void goToTab(int index) {
+    _selectedTab = index;
+    notifyListeners();
+  }
+
+  void goToRecipes() {
+    _selectedTab = FooderlichTab.recipes;
+    notifyListeners();
+  }
+
+  void logout() {
+    _loggedIn = false;
+    _onBoardingComplete = false;
+    _initialized = false;
+    _selectedTab = 0;
+
+    initializeApp();
+    notifyListeners();
+  }
+}
