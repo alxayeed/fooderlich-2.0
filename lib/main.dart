b3bc3d020c2ac0ebf17dd0cacecfd950eb2b6253
alsayeed <alxayeed@gmail.com>
2022-11-03 19:31:51 +0600

    logger added

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,17 @@ import 'package:logging/logging.dart';
 import 'ui/main_screen.dart';
 
 Future<void> main() async {
+  _setupLogging();
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
+void _setupLogging() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
