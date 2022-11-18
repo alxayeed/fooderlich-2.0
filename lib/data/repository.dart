86604ed0bcd9b12335c0bf14188e52946f88523b
alsayeed <alxayeed@gmail.com>
2022-11-18 10:57:41 +0600

    repository and memory repository added

---
+++ lib/data/repository.dart
@@ -0,0 +1,25 @@
+import 'dart:core';
+
+import 'models/models.dart';
+
+abstract class Repository{
+  //find methods
+  List<Recipe> findAllRecipes();
+  Recipe findRecipeById(int id);
+  List<Ingredient> findAllIngredients();
+  List<Ingredient> findRecipeIngredients(int recipeId);
+
+  //insert methods
+  int insertRecipe(Recipe recipe);
+  List<int> insertIngredients(List<Ingredient> ingredients);
+
+  //delete methods
+  void deleteRecipe(Recipe recipe);
+  void deleteIngredient(Ingredient ingredient);
+  void deleteIngredients(List<Ingredient> ingredients);
+  void deleteRecipeIngredients(int recipeId);
+
+  //init and closing methods
+  Future init();
+  void close();
+}
